fe5b609c591c66022e07bea2852e67f0a2a73de4
Ramy Kader <ramy.kader@esprit.tn>
2018-09-29 11:19:34 +0100

    Init - MicroAuthenticateCommit

---
+++ microauthenticate/src/main/java/com/tnbank/microauthenticate/MicroauthenticateApplication.java
@@ -0,0 +1,12 @@
+package com.tnbank.microauthenticate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroauthenticateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroauthenticateApplication.class, args);
+	}
+}
---
+++ microauthenticate/src/test/java/com/tnbank/microauthenticate/MicroauthenticateApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tnbank.microauthenticate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroauthenticateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
